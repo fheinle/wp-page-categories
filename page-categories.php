1c0756f96b10f5e50a3165b0e399458290cdf033
Florian Heinle <launchpad@planet-tiax.de>
2016-12-12 18:04:33 +0100

    also allow excerpts for pages

--- page-categories.php
+++ page-categories.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Page Categories
-Plugin URI: https://github.com/fheinle/wp-page-categories
+Plugin URI: https://github.com/wp-page-categories
 Description: Adds categories taxonomy to pages
 Version: 1.0
 Author: Florian Heinle
@@ -12,7 +12,12 @@ License: GPL2
 add_action('admin_init', 'register_categories_taxonomy');
 
 function register_categories_taxonomy() {
-    register_taxonomy_for_object_type('category', 'page');
-    add_post_type_support('page', 'category');
+	register_taxonomy_for_object_type('category', 'page');
+	add_post_type_support('page', 'category');
+}
+
+add_action('init', 'add_excerpts_to_pages');
+function add_excerpts_to_pages() {
+	add_post_type_support('page', 'excerpt');
 }
 ?>
